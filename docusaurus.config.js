aa1ed12a9b5d22fdcf8be228c1bc2ecce7d86278
DawnMagnet <axccjqh@qq.com>
2022-01-15 11:09:02 +0800

    change INC

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} DawnMagnet, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
